9803ead970eaaeb38d712b7533f82dc2d8c4a00e
Samyak Bambole <64409597+samyakbambole@users.noreply.github.com>
2020-06-27 13:43:54 +0530

    Add files via upload

---
+++ public/sketch.js
@@ -0,0 +1,45 @@
+var socket; 
+
+function setup() {
+  	// put setup code here
+  	createCanvas(windowWidth, windowHeight); 
+  	background(51); 
+
+  	socket = io.connect('http://localhost:3000'); 
+  	socket.on('mouse', newDrawing); 
+}
+
+function newDrawing(data) {
+	fill(25, 255, 255); 
+  	noStroke(); 
+  	ellipse(data.x, data.y, 50, 50); 
+     function keyPressed() {
+          if (key = 'c') {
+               background(51); 
+          }
+     }
+}
+
+function mouseDragged() {
+console.log('Sending - ' + mouseX + ' , ' + mouseY); 
+
+	var data = {
+		x: mouseX, 
+		y: mouseY
+	}
+	socket.emit('mouse', data); 
+
+  	fill(25, 25, 255); 
+  	noStroke(); 
+  	ellipse(mouseX, mouseY, 50, 50); 	
+}
+
+function keyPressed() {
+     if (key = 'c') {
+          background(51); 
+     } 
+}
+
+function draw() {
+  	// put drawing code here
+}
